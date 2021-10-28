2a4a4468deb6022bb575b99ad1933d5b76e11e34
sasangeo <91782933+sasangeo@users.noreply.github.com>
2021-10-28 15:38:49 +0700

    Update speed.js

--- plugins/speed.js
+++ plugins/speed.js
@@ -40,23 +40,13 @@ let handler = async (m, { conn }) => {
   let speed = neww - old
   let txt = `
 Merespon dalam ${speed} millidetik
-
 💻 *Server Info* :
 RAM: ${format(os.totalmem() - os.freemem())} / ${format(os.totalmem())}
-
-_NodeJS Memory Usage_
-${'```' + Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.map(v => v.length)), ' ')}: ${format(used[key])}`).join('\n') + '```'}
-
-${cpus[0] ? `_Total CPU Usage_
-${cpus[0].model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}
-
-_CPU Core(s) Usage (${cpus.length} Core CPU)_
-${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}` : ''}
 `.trim()
   m.reply(txt)
 }
-handler.help = ['ping']
+handler.help = ['speed']
 handler.tags = ['info']
 
-handler.command = /^(ping|speed)$/i
+handler.command = /^speed$/i
 module.exports = handler

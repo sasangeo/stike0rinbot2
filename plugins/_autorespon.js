a2ebd8dc860c19d7657e168f0ae55b4d1eae1282
sasangeo <91782933+sasangeo@users.noreply.github.com>
2021-10-28 15:48:17 +0700

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -32,7 +32,6 @@ handler.all = async function (m, { isBlocked }) {
 ├ 30 Hari / Rp 10,000
 └────
 
-https://github.com/ariffb25/stikerinbot
 `.trim(), '© stikerin', 'Pemilik Bot', ',owner', m)
     }
 
